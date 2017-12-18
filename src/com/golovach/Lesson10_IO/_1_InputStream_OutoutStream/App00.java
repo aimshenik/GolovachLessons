fabe0dc24d1bd00dc3a9274eebce3142cd669fe7
Andrey <a.imshenick@gmail.com>
2017-12-18 17:55:22 +0300

    Start 10 lesson

---
+++ src/com/golovach/Lesson10_IO/_1_InputStream_OutoutStream/App00.java
@@ -0,0 +1,10 @@
+package com.golovach.Lesson10_IO._1_InputStream_OutoutStream;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class App00 {
+    public static void main(String[] args) {
+        
+    }
+}
